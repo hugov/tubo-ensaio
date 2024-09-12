51546ea30fcecfcc9d3132acffda17d4661aa0ed
Vitor Hugo Brito de Oliveira <hugov1983@gmail.com>
2024-09-12 12:29:37 -0300

    Adicionado a documentação básica do swagger

--- api-fastify/src/server.ts
+++ api-fastify/src/server.ts
@@ -1,6 +1,8 @@
 import Fastify, { FastifyRequest, FastifyReply } from "fastify";
 import fjwt, { FastifyJWT } from '@fastify/jwt';
 import fCookie from '@fastify/cookie';
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUi from "@fastify/swagger-ui";
 
 import userRoutes from "./modules/user/user.route";
 import { userSchemas } from './modules/user/user.schema';
@@ -19,7 +21,47 @@ fastify.addHook('preHandler', (req, res, next) => {
 fastify.register(fCookie, {
     secret: process.env.COOKIE_SECRET || 'imvinojan02061999xxxx',
     hook: 'preHandler',
-})
+});
+
+const swaggerOptions = {
+    routePrefix: '/docs',
+    swagger: {
+        info: {
+            title: 'Fastify Prisma REST API',
+            description: 'A REST API built with Fastify, Prisma and TypeScript',
+            version: '1.0.0',
+            contact: {
+                name: "Vitor Hugo Oliveira",
+                //url: "https://www.hugov.com.br",
+                email: "hugov1983@gmail.com"
+            },
+        },
+        externalDocs: {
+            url: 'https://github.com/hugov/tubo-ensaio',
+            description: 'Pasta - api-fastify',
+        },
+        host: '0.0.0.0:3000',
+        basePath: '/',
+        schemes: ['http', 'https'],
+        consumes: ['application/json'],
+        produces: ['application/json'],
+    },
+    uiConfig: {
+        docExpansion: 'none', // expand/not all the documentations none|list|full
+        deepLinking: true,
+    },
+    staticCSP: false,
+    transformStaticCSP: (header: any) => header,
+    exposeRoute: true
+};
+
+const swaggerUiOptions = {
+    routePrefix: "/docs",
+    exposeRoute: true,
+};
+
+fastify.register(fastifySwagger, swaggerOptions);
+fastify.register(fastifySwaggerUi, swaggerUiOptions);
 
 fastify.decorate(
     'authenticate',
@@ -47,49 +89,15 @@ async function main() {
         fastify.addSchema(schema);
     };
 
-    /*
-    fastify.register(require('@fastify/swagger'), {});
-    fastify.register(require('@fastify/swagger-ui'), {
-        routePrefix: '/docs',
-        swagger: {
-            info: {
-                title: 'Fastify Prisma REST API',
-                description: 'A REST API built with Fastify, Prisma and TypeScript',
-                version: '1.0.0',
-                contact: {
-                    name: "Vinojan Abhimanyu",
-                    url: "https://vinojan.online",
-                    email: "imvinojanv@gmail.com"
-                },
-            },
-            externalDocs: {
-                url: 'https://github.com/imvinojanv/fastify-prisma-rest-api',
-                description: 'Fastify Tutorial source code is on GitHub',
-            },
-            host: '0.0.0.0:3000',
-            basePath: '/',
-            schemes: ['http', 'https'],
-            consumes: ['application/json'],
-            produces: ['application/json'],
-        },
-        uiConfig: {
-            docExpansion: 'none', // expand/not all the documentations none|list|full
-            deepLinking: true,
-        },
-        staticCSP: false,
-        transformStaticCSP: (header: any) => header,
-        exposeRoute: true
-    });
-    
     // Executes Swagger
     fastify.ready(err => {
-        if (err) throw err
-        fastify.swagger()
-    })
-    */
+        if (err) 
+            throw err
+        
+            fastify.swagger();
+    });
     
-    fastify.register(userRoutes, { prefix: 'api/users' })           // user routes
-    //fastify.register(productRoutes, { prefix: 'api/products' })     // product routes
+    fastify.register(userRoutes, { prefix: 'api/users' });
 
     try {
         await fastify.listen({ port: 3000, host: "0.0.0.0" });
